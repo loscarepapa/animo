2a8951000194c845f843bcd5921527f1e0ca958a
loscarepapa <loscarepapap@gmail.com>
2019-04-15 17:54:42 -0500

    Creation of veriables containers of attributes

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 
-var image_template = document.getElementById("lienzo")
-var images_totals = 1
+var lienzo = document.getElementById("lienzo")
+var total_imagenes = 0
+var total_de_capas = []
+var num_atributos_de_imagenes = []
+var atributos_de_imagenes = []
+
+
 function mostrar() {
     var archivo = document.getElementById("file").files[0];
     var reader = new FileReader();
@@ -8,10 +13,14 @@ function mostrar() {
         reader.readAsDataURL(archivo);
         reader.onloadend = function () {
             
-            image_template.innerHTML = `<img id="${images_totals}" src="${reader.result}">`
-            images_totals++
+            lienzo.innerHTML += `<img id="${total_imagenes}" src="${reader.result}">`
+            total_de_capas[total_imagenes]
+            console.log(total_de_capas)
+            total_imagenes++
+            console.log(archivo)
         }
     } else {
         console.log("Hubo un error")
     }
 }
+
