786b36cba2296914387b9f048bf01e888f5795f0
loscarepapa <loscarepapap@gmail.com>
2019-04-19 20:07:18 -0500

    Function inproved

--- main.js
+++ main.js
@@ -1,267 +1,292 @@
-var lienzo = document.getElementById("lienzo")
-var total_imagenes = 0
-var total_de_capas = []
-var num_atributos_de_imagenes = []
-var atributos_de_imagenes = []
+var lienzo = document.getElementById("lienzo");
+var total_imagenes = 0;
+var total_de_capas = [];
+var num_atributos_de_imagenes = [];
+var atributos_de_imagenes = [];
 
-var z_index = 1
+var z_index = 1;
 
 function mostrar() {
-    var archivo = document.getElementById("file").files[0];
-    var reader = new FileReader();
-    if (file) {
-        reader.readAsDataURL(archivo);
-        reader.onloadend = function () {
-
-            lienzo.innerHTML += `<img 
+  var archivo = document.getElementById("file").files[0];
+  var reader = new FileReader();
+  if (file) {
+    reader.readAsDataURL(archivo);
+    reader.onloadend = function() {
+      lienzo.innerHTML += `<img 
             id="${total_imagenes}"
             style="left:0px;top:0px;transform:rotateZ(0deg);width:100px;height:100px;opacity:1;z-index:${z_index};" 
             class="img"
             width="100px" 
             src="${reader.result}"
             onclick="watch(this.id)"
-            draggable="true"> `;
+            draggable="false"> `;
 
-            z_index++
-            total_de_capas[total_imagenes] = total_imagenes
-            console.log(total_de_capas)
-            total_imagenes++
+      z_index++;
+      total_de_capas[total_imagenes] = total_imagenes;
+      console.log(total_de_capas);
+      total_imagenes++;
+    };
+  } else {
+    console.log("Hubo un error");
+  }
 
-        }
-    } else {
-        console.log("Hubo un error")
+  if (img_puntual) {
+    rotacion.style.display = "none";
+    alto.style.display = "none";
+    ancho.style.display = "none";
+    x.style.display = "none";
+    y.style.display = "none";
+    opacidad.style.display = "none";
+    zindex.style.display = "none";
+    id_seleccionador.value = "";
+    id_seleccionador.style.display = "none";
+    tabla.style.display = "none";
+
+    rotacion.value = "";
+    alto.value = "";
+    ancho.value = "";
+    x.value = "";
+    y.value = "";
+    opacidad.value = "";
+    zindex.value = "";
+
+    for (let i = 0; i < 8; i++) {
+      document.getElementById(`title_attribute${i}`).style.display = "none";
     }
-    if (img_puntual) {
 
-        rotacion.style.display = "none"
-        alto.style.display = "none"
-        ancho.style.display = "none"
-        x.style.display = "none"
-        y.style.display = "none"
-        opacidad.style.display = "none"
-        zindex.style.display = "none"
-        id_seleccionador.value = ""
-        id_seleccionador.style.display = "none"
-        tabla.style.display = "none"
-
-        rotacion.value = ""
-        alto.value = ""
-        ancho.value = ""
-        x.value = ""
-        y.value = ""
-        opacidad.value = ""
-        zindex.value = ""
-
-        for (let i = 0; i < 8; i++) {
-            document.getElementById(`title_attribute${i}`).style.display = "none"
-        }
-
-        img_puntual.className = "img"
-        img_puntual = 0
-
-    }
+    img_puntual.className = "img";
+    img_puntual.setAttribute("draggable", "false");
+    img_puntual = 0;
+  }
 }
 
-var img_puntual
-var id_seleccionador = document.getElementById("id_imagen")
-var tabla = document.getElementById("tabla")
+var img_puntual;
+var id_seleccionador = document.getElementById("id_imagen");
+var tabla = document.getElementById("tabla");
 var rotacion = document.getElementById("r"),
-    alto = document.getElementById("al"),
-    ancho = document.getElementById("an"),
-    x = document.getElementById("x"),
-    y = document.getElementById("y"),
-    opacidad = document.getElementById("o"),
-    zindex = document.getElementById("z")
+  alto = document.getElementById("al"),
+  ancho = document.getElementById("an"),
+  x = document.getElementById("x"),
+  y = document.getElementById("y"),
+  opacidad = document.getElementById("o"),
+  zindex = document.getElementById("z");
 
 function watch(id) {
+  if (document.getElementById(id) == img_puntual) {
+    document.getElementById(id).setAttribute("draggable", "false");
 
-    if (document.getElementById(id) == img_puntual) {
-        img_puntual.className = "img"
-        img_puntual = 0
-        id_seleccionador.value = ""
-        id_seleccionador.style.display = "none"
-        tabla.style.display = "none"
+    img_puntual.className = "img";
+    img_puntual.setAttribute("draggable", "false");
 
-        rotacion.style.display = "none"
-        alto.style.display = "none"
-        ancho.style.display = "none"
-        x.style.display = "none"
-        y.style.display = "none"
-        opacidad.style.display = "none"
-        zindex.style.display = "none"
+    img_puntual = 0;
+    id_seleccionador.value = "";
+    id_seleccionador.style.display = "none";
+    tabla.style.display = "none";
 
-        rotacion.value = ""
-        alto.value = ""
-        ancho.value = ""
-        x.value = ""
-        y.value = ""
-        opacidad.value = ""
-        zindex.value = ""
+    rotacion.style.display = "none";
+    alto.style.display = "none";
+    ancho.style.display = "none";
+    x.style.display = "none";
+    y.style.display = "none";
+    opacidad.style.display = "none";
+    zindex.style.display = "none";
 
-        for (let i = 0; i < 8; i++) {
-            document.getElementById(`title_attribute${i}`).style.display = "none"
-        }
+    rotacion.value = "";
+    alto.value = "";
+    ancho.value = "";
+    x.value = "";
+    y.value = "";
+    opacidad.value = "";
+    zindex.value = "";
 
-    } else {
-
-        id_seleccionador.value = id
-
-        var img_watch = document.getElementById(id)
-
-        var style_rotate = img_watch.style.transform
-        var style_alto = img_watch.style.height
-        var style_ancho = img_watch.style.width
-        var style_x = img_watch.style.left
-        var style_y = img_watch.style.top
-        var style_opacidad = img_watch.style.opacity
-        var style_zindex = img_watch.style.zIndex
-
-
-        if (img_puntual) {
-
-            img_puntual.className = "img"
-            img_puntual = img_watch
-            img_watch.className = "img_watch"
-            id_seleccionador.style.display = "inline-block"
-            tabla.style.display = "inline-block"
-
-            rotacion.style.display = "inline-block"
-            alto.style.display = "inline-block"
-            ancho.style.display = "inline-block"
-            x.style.display = "inline-block"
-            y.style.display = "inline-block"
-            opacidad.style.display = "inline-block"
-            zindex.style.display = "inline-block"
-
-            for (let i = 0; i < 8; i++) {
-                document.getElementById(`title_attribute${i}`).style.display = "inline"
-            }
-
-            rotacion.value = (style_rotate = style_rotate.replace("rotateZ(", ""), style_rotate.replace("deg)", ""))
-            alto.value = (style_alto = style_alto.replace("px", ""))
-            ancho.value = (style_ancho = style_ancho.replace("px", ""))
-            x.value = (style_x = style_x.replace("px", ""))
-            y.value = (style_y = style_y.replace("px", ""))
-            opacidad.value = style_opacidad
-            zindex.value = style_zindex
-            // console.log(style_x)
-
-        } else {
-
-            img_puntual = img_watch
-            img_watch.className = "img_watch"
-            id_seleccionador.style.display = "inline-block"
-            tabla.style.display = "inline-block"
-
-            rotacion.style.display = "inline-block"
-            alto.style.display = "inline-block"
-            ancho.style.display = "inline-block"
-            x.style.display = "inline-block"
-            y.style.display = "inline-block"
-            opacidad.style.display = "inline-block"
-            zindex.style.display = "inline-block"
-
-            for (let i = 0; i < 8; i++) {
-                document.getElementById(`title_attribute${i}`).style.display = "inline"
-            }
-
-            rotacion.value = (style_rotate = style_rotate.replace("rotateZ(", ""), style_rotate.replace("deg)", ""))
-            alto.value = (style_alto = style_alto.replace("px", ""))
-            ancho.value = (style_ancho = style_ancho.replace("px", ""))
-            x.value = (style_x = style_x.replace("px", ""))
-            y.value = (style_y = style_y.replace("px", ""))
-            opacidad.value = style_opacidad
-            zindex.value = style_zindex
-            // console.log(style_x)
-
-        }
+    for (let i = 0; i < 8; i++) {
+      document.getElementById(`title_attribute${i}`).style.display = "none";
     }
-    // console.log(img_watch.style.transform)
-    // console.log(img_puntual)
+  } else {
+    id_seleccionador.value = id;
+
+    var img_watch = document.getElementById(id);
+
+    var style_rotate = img_watch.style.transform;
+    var style_alto = img_watch.style.height;
+    var style_ancho = img_watch.style.width;
+    var style_x = img_watch.style.left;
+    var style_y = img_watch.style.top;
+    var style_opacidad = img_watch.style.opacity;
+    var style_zindex = img_watch.style.zIndex;
+
+    if (img_puntual) {
+      img_puntual.className = "img";
+
+      img_puntual.setAttribute("draggable", "false");
+
+      img_puntual = img_watch;
+
+      img_watch.className = "img_watch";
+
+      img_watch.setAttribute("draggable", "true");
+
+      id_seleccionador.style.display = "inline-block";
+      tabla.style.display = "inline-block";
+
+      rotacion.style.display = "inline-block";
+      alto.style.display = "inline-block";
+      ancho.style.display = "inline-block";
+      x.style.display = "inline-block";
+      y.style.display = "inline-block";
+      opacidad.style.display = "inline-block";
+      zindex.style.display = "inline-block";
+
+      for (let i = 0; i < 8; i++) {
+        document.getElementById(`title_attribute${i}`).style.display = "inline";
+      }
+
+      rotacion.value = ((style_rotate = style_rotate.replace("rotateZ(", "")),
+      style_rotate.replace("deg)", ""));
+      alto.value = style_alto = style_alto.replace("px", "");
+      ancho.value = style_ancho = style_ancho.replace("px", "");
+      x.value = style_x = style_x.replace("px", "");
+      y.value = style_y = style_y.replace("px", "");
+      opacidad.value = style_opacidad;
+      zindex.value = style_zindex;
+      // console.log(style_x)
+    } else {
+      img_puntual = img_watch;
+      img_watch.className = "img_watch";
+
+      img_watch.setAttribute("draggable", "true");
+
+      id_seleccionador.style.display = "inline-block";
+      tabla.style.display = "inline-block";
+
+      rotacion.style.display = "inline-block";
+      alto.style.display = "inline-block";
+      ancho.style.display = "inline-block";
+      x.style.display = "inline-block";
+      y.style.display = "inline-block";
+      opacidad.style.display = "inline-block";
+      zindex.style.display = "inline-block";
+
+      for (let i = 0; i < 8; i++) {
+        document.getElementById(`title_attribute${i}`).style.display = "inline";
+      }
+
+      rotacion.value = ((style_rotate = style_rotate.replace("rotateZ(", "")),
+      style_rotate.replace("deg)", ""));
+      alto.value = style_alto = style_alto.replace("px", "");
+      ancho.value = style_ancho = style_ancho.replace("px", "");
+      x.value = style_x = style_x.replace("px", "");
+      y.value = style_y = style_y.replace("px", "");
+      opacidad.value = style_opacidad;
+      zindex.value = style_zindex;
+    }
+  }
 }
 
 function change_id(id) {
-    watch(id)
+  watch(id);
 }
 
 function attr(value, id) {
-    switch (id) {
-        case "x":
-            img_puntual.style.left = `${value}px`
-            break;
+  switch (id) {
+    case "x":
+      img_puntual.style.left = `${value}px`;
+      break;
 
-        case "y":
-            img_puntual.style.top = `${value}px`
-            break;
+    case "y":
+      img_puntual.style.top = `${value}px`;
+      break;
 
-        case "r":
-            img_puntual.style.transform = `rotateZ(${value}deg)`
-            break;
-        case "al":
-            img_puntual.style.height = `${value}px`
-            break;
-        case "an":
-            img_puntual.style.width = `${value}px`
-            break;
-        case "o":
-            img_puntual.style.opacity = value
-            break;
-        case "z":
-            img_puntual.style.zIndex = value
-            break;
-        default:
-            break;
-    }
+    case "r":
+      img_puntual.style.transform = `rotateZ(${value}deg)`;
+      break;
+    case "al":
+      img_puntual.style.height = `${value}px`;
+      break;
+    case "an":
+      img_puntual.style.width = `${value}px`;
+      break;
+    case "o":
+      img_puntual.style.opacity = value;
+      break;
+    case "z":
+      img_puntual.style.zIndex = value;
+      break;
+    default:
+      break;
+  }
 }
 
+var movimiento_x, movimiento_y;
+var img_movement;
 
-var movimiento_x, movimiento_y
-var img_movement
-
-function captura_movimiento(){
-    movimiento_x = window.event.clientX
-    movimiento_y = window.event.clientY
-    console.log(window.event.clientX);
+function captura_movimiento() {
+  movimiento_x = window.event.clientX;
+  movimiento_y = window.event.clientY;
+  console.log(window.event.clientX);
 }
 
 var proceso_Id = function() {
-        movimiento_x = window.event.clientX;
-        movimiento_y = window.event.clientY;
-        console.log(window.event.clientX);
+  movimiento_x = window.event.clientX;
+  movimiento_y = window.event.clientY;
+  console.log(window.event.clientX);
 };
-
 
 //System of a drag
 function movement() {
+  movimiento_x = window.event.clientX;
+  movimiento_y = window.event.clientY;
 
-    movimiento_x = window.event.clientX
-    movimiento_y = window.event.clientY
-
-    if (img_movement) {
-        if (img_puntual) {
-            img_puntual.style.left = `${movimiento_x - (img_puntual.style.width.replace("px", "") / 2)}px`
-            img_puntual.style.top = `${movimiento_y - (img_puntual.style.height.replace("px", "") / 2)}px`
-
-        }
+  if (img_movement) {
+    if (img_puntual) {
+      img_puntual.style.left = `${movimiento_x -
+        img_puntual.style.width.replace("px", "") / 2}px`;
+      img_puntual.style.top = `${movimiento_y -
+        img_puntual.style.height.replace("px", "") / 2}px`;
     }
-
+  }
 }
 
-var img_press
+var img_press;
+var position_mouse_img_x, position_mouse_img_y;
 
 document.addEventListener("dragstart", function(event) {
+  position_mouse_img_x =
+    document.getElementById(event.target.id).style.left.replace("px", "") +
+    document.getElementById(event.target.id).style.width.replace("px", "");
+  position_mouse_img_y =
+    document.getElementById(event.target.id).style.top.replace("px", "") +
+    document.getElementById(event.target.id).style.height.replace("px", "");
+  console.log(
+    document.getElementById(event.target.id).style.width.replace("px", "")
+  );
 
-    img_press = document.getElementById(event.target.id);
-
+  img_press = document.getElementById(event.target.id);
 });
 
+// document.addEventListener("dragstart", function(event) {
+//   position_mouse_img_x = parseInt(
+//     document.getElementById(event.target.id).style.left.replace("px", "") +
+//       document.getElementById(event.target.id).style.width.replace("px", "")
+//   );
+
+//   position_mouse_img_y = parseInt(
+//     document.getElementById(event.target.id).style.top.replace("px", "") +
+//       document.getElementById(event.target.id).style.height.replace("px", "")
+//   );
+//   console.log(
+//     document.getElementById(event.target.id).style.width.replace("px", "")
+//   );
+
+//   img_press = position_mouse_img_y;
+// });
 
 document.addEventListener("dragend", function(event) {
-  
-    if(img_puntual){
-        img_press.style.left = `${window.event.clientX - (img_puntual.style.width.replace("px", "") / 2)}px`;
-        img_press.style.top = `${window.event.clientY - (img_puntual.style.height.replace("px", "") / 2)}px`
-    }
-
+  if (img_press && img_press.className === "img_watch") {
+    img_press.style.left = `${window.event.clientX -
+      img_puntual.style.width.replace("px", "") / 2}px`;
+    img_press.style.top = `${window.event.clientY -
+      img_puntual.style.height.replace("px", "") / 2}px`;
+  }
 });
 //
